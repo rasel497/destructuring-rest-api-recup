5bccbcca3352eb212203329d38fbd3501adeeb9f
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-20 15:10:38 +0600

    show flags without destructuring initial Option-01

--- js/countries.js
+++ js/countries.js
@@ -1,8 +1,28 @@
 // alert('Im rest your Boss!')
+
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayCountries(data));
+}
+
+const displayCountries = countries => {
+    console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
+}
+
+const getCountryHTML = country => {
+    // show flags without destructuring initial Option--01
+    return `
+    <div class="country">
+        <h2>${country.name.common}</h2>
+        <img src="${country.flags.png}">
+    </div>
+    `
+    // <></>
 }
 
 loadCountries();
